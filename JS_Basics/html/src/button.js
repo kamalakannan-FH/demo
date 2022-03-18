27e6376e0c5f5f1c56d312e556e62fd7a84a216e
Dhinesh-T2S <dhinesh.m@touch2success.com>
2022-03-18 13:56:02 +0530

    init

--- JS_Basics/html/src/button.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.btn1').addEventListener('click',function(){
- document.getElementById('msg').textContent = 'Bye'
-})
-
-
-// document.querySelector('.btn1').addEventListener('click',function(){
-//     document.getElementById('msg').innerHTML = 'Bye'
-//    })
--- JS_Basics/html/src/index.js
+++
@@ -1,7 +0,0 @@
-// let a = document.querySelector('.one')
-// // let a = document.querySelector('#one').textContent
-
-
-// console.log(a)
-
-alert('hellllloooooooo')
